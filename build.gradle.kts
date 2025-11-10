2c3d1bda031e9ab2dd8fb937ed6355201979d1d9
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2025-11-10 15:36:42 +0100

    4.15.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ description = "UpgradeableSpawnersAPI"
 
 plugins {
     `java-library`
-    id("io.github.goooler.shadow").version("8.1.8")
+    id("com.gradleup.shadow").version("9.2.2")
     `maven-publish`
 }
 
